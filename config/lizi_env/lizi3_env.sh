fdce1593f034f3fdb00992073e4410bedbcd58c9
robotican <support@robotican.net>
2014-03-11 11:16:29 +0200

    stack

--- config/lizi_env/lizi3_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export ROS_HOSTNAME="192.168.0.103"
-export ROS_IP="192.168.0.103"
-export ROS_MASTER_URI="http://192.168.0.100:11311"
-. ~/catkin_ws/devel/setup.sh
-exec "$@"
--- config/ros_lib make libraries.sh
+++
@@ -1,5 +0,0 @@
-cd ~/catkin_ws
-catkin_make
-cd ~/Arduino/libraries
-rm -rf ros_lib
-rosrun rosserial_arduino make_libraries.py .
